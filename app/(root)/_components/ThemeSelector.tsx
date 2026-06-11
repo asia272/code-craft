8b2047176a4987996b87691fc761cce86ae8c080
asia272 <asiaashraf7272@gmail.com>
2026-06-11 13:16:11 +0500

    working on LanguageSelector

--- app/(root)/_components/ThemeSelector.tsx
+++ app/(root)/_components/ThemeSelector.tsx
@@ -4,25 +4,24 @@ import { useCodeEditorStore } from "@/store/useCodeEditorStore";
 import React, { useEffect, useRef, useState } from "react";
 import { THEMES } from "../_constants";
 import { AnimatePresence, motion } from "framer-motion";
-import { CircleOff, Cloud, GiftIcon, Laptop, Moon, Palette, Sun } from "lucide-react";
-
+import { CircleOff, Cloud, Laptop, Moon, Palette, Sun } from "lucide-react";
+// import useMounted from "@/hooks/useMounted";
 
 const THEME_ICONS: Record<string, React.ReactNode> = {
     "vs-dark": <Moon className="size-4" />,
     "vs-light": <Sun className="size-4" />,
-    "github-dark": <GiftIcon className="size-4" />,
+
     monokai: <Laptop className="size-4" />,
     "solarized-dark": <Cloud className="size-4" />,
 };
 
 function ThemeSelector() {
     const [isOpen, setIsOpen] = useState(false);
+    // const mounted = useMounted();
     const { theme, setTheme } = useCodeEditorStore();
     const dropdownRef = useRef<HTMLDivElement>(null);
     const currentTheme = THEMES.find((t) => t.id === theme);
 
-
-    //When we click outside the dropdown
     useEffect(() => {
         const handleClickOutside = (event: MouseEvent) => {
             if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
@@ -34,7 +33,7 @@ function ThemeSelector() {
         return () => document.removeEventListener("mousedown", handleClickOutside);
     }, []);
 
-    //   if (!mounted) return null;
+    // if (!mounted) return null;
 
     return (
         <div className="relative" ref={dropdownRef}>
